fee712522c97c499057b0b6c037bdd75e531275f
caqqmarina <chiara.aqmarina@ui.ac.id>
2024-11-11 02:14:58 +0700

    simple landing page

---
+++ main/urls.py
@@ -0,0 +1,17 @@
+# main/urls.py
+from django.contrib import admin
+from django.urls import path
+from main import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.landing_page, name='landing'),
+    path('homepage/', views.homepage, name='homepage'),  # Separate URL for homepage
+    path('login/', views.login_view, name='login'),
+    path('register_landing/', views.register_landing, name='register_landing'),
+    # path('login/', views.login_view, name='login'),
+    # path('register/', views.register_view, name='register'),
+    # path('logout/', views.logout_view, name='logout'),
+
+]
+
--- main/views.py
+++ main/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # main/views.py
 def homepage(request):
     return render(request, 'homepage.html')
+
+def landing_page(request):
+    return render(request, 'landing.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def register_landing(request):
+    return render(request, 'register_landing.html')
