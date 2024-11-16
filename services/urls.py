64b94f19e330a6abd7abc424d03c80fd5e2e0acf
wuyu0107 <mkim050107@gmail.com>
2024-11-17 02:11:19 +0700

    Added temporary data in json, implement service category webpage according to id

--- services/urls.py
+++ services/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from services.views import list_services
+from services.views import subcategory
 
 app_name = 'services'
 
 urlpatterns = [
-    path('', list_services, name='list_services'),  # URL: /services/
+    path('<int:subcategory_id>/', subcategory, name='category_services'),
 ]
