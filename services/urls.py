50d977afbdfd95f6cf7434e4bc1d54627744499c
neaurellia <catherineaurellia@gmail.com>
2024-11-17 23:47:42 +0700

    implemented testimonials

--- services/urls.py
+++ services/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from services.views import subcategory
+from . import views
 
 app_name = 'services'
 
 urlpatterns = [
     path('<int:subcategory_id>/', subcategory, name='category_services'),
+    path('subcategory/<int:subcategory_id>/testimonials/', views.testimonials, name='testimonials'),
+    path('subcategory/<int:subcategory_id>/create-testimonial/', views.create_testimonial, name='create_testimonial'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from services.models import Subcategory, Worker, Testimonial, ServiceSession, ServiceCategory
 from services.forms import SubcategoryForm, CategoryForm
+from django.contrib.auth.decorators import login_required
+from .forms import TestimonialForm
 
 def subcategory(request, subcategory_id=None):
     subcategory = get_object_or_404(Subcategory, pk=subcategory_id)
@@ -17,3 +19,23 @@ def subcategory(request, subcategory_id=None):
     }
 
     return render(request, 'subcategory.html', context)
+
+@login_required
+def create_testimonial(request, subcategory_id):
+    subcategory = Subcategory.objects.get(id=subcategory_id)
+    if request.method == 'POST':
+        form = TestimonialForm(request.POST)
+        if form.is_valid():
+            testimonial = form.save(commit=False)
+            testimonial.user = request.user
+            testimonial.subcategory = subcategory
+            testimonial.save()
+            return redirect('subcategory_detail', subcategory_id=subcategory.id)  # Redirect to subcategory detail page
+    else:
+        form = TestimonialForm()
+    return render(request, 'create_testimonial.html', {'form': form, 'subcategory': subcategory})
+
+def testimonials(request, subcategory_id):
+    subcategory = Subcategory.objects.get(id=subcategory_id)
+    testimonials = Testimonial.objects.filter(subcategory=subcategory).order_by('-date')
+    return render(request, 'subcategory_detail.html', {'subcategory': subcategory, 'testimonials': testimonials})
